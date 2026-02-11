aaaf89dc67da4492f14834f12aaa593c74e81373
utterglobalrestoration <utterglobalrestoration@gmail.com>
2026-02-11 11:51:19 -0600

    For real this time

---
+++ Assets/PlayerCollision.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    private void OnCollisionEnter()
+    {
+        Debug.Log("We Hit something");
+        
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 1000f;
+    public float horizontalForce = 500f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello World");
+        rb.useGravity = true;
+        rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rb.AddForce(0, 0, forwardForce*Time.deltaTime);
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(horizontalForce * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-1*horizontalForce * Time.deltaTime, 0, 0);
+        }
+    }
+}
